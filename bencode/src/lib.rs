df02f2655b62ba2c7a07d1fc51052756c91684ee
Adrian Plavka <adrian.plavka@gmail.com>
2018-05-22 22:10:07 +0200

    Changed the process of handling bencode parsing, make use of std::io::Cursor

--- bencode/src/lib.rs
+++ bencode/src/lib.rs
@@ -1,138 +1,186 @@
 
 #![feature(test)]
 
+// TODO: Remove these after the successful implementation.
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
 pub mod bencode {
     use std;
+    use std::result::{Result};
+    use std::io::{Read};
 
-    const NUM_DELIMITER: char   = 'i';
-    const DICT_DELIMITER: char  = 'd';
-    const LIST_DELIMITER: char  = 'l';
-    const END_DELIMITER: char   = 'e';
-    const COL_DELIMITER: char   = ':';
+    pub fn decode(data: &str) -> Result<Value, DecodeError> {
+        // TODO: Make use of decode functions.
+        unimplemented!()
+    }
+
+    pub fn decode_from<R: std::io::Read + std::io::Seek>(reader: R) -> Result<Value, DecodeError> {
+        unimplemented!()
+    }
 
     #[derive(Debug, PartialEq)]
-    pub enum Error {
+    pub enum DecodeError {
         Invalid,
         UnexpectedSymbol,
         UnexpectedEnd,
         UnsupportedType,
+        EOF,
+        ParseError,
         Unknown
     }
 
-    pub type Result<T> = std::result::Result<T, Error>;
-
     #[derive(Debug, PartialEq, Eq, Hash)]
-    pub struct Bytes(Vec<u8>);
+    pub struct Byte(u8);
+    
+    #[derive(Debug, PartialEq, Eq, Hash)]
+    pub struct Bytes(Vec<Byte>);
 
     #[derive(Debug, PartialEq)]
-    pub enum Type {
-        Num(i64),
+    pub enum Value {
+        Int(i64),
         Str(Bytes),
-        List(Vec<Type>),
-        Dict(std::collections::HashMap<Bytes, Type>),
-        None
-    }
-
-    #[derive(Debug, PartialEq)]
-    enum ParseState {
-        AtNum,
-        AtStr,
-        AtList,
-        AtDict,
+        List(Vec<Value>),
+        Dict(std::collections::BTreeMap<Bytes, Value>),
         None
     }
 
     #[derive(Debug)]
-    pub struct Parser {
-        pos: u64,
-        state: ParseState
+    struct Decoder<'a> {
+        data: std::io::Cursor<&'a [u8]>
     }
 
-    impl Parser {
-        pub fn decode_str(data: &str) -> Result<Type> {
-            unimplemented!()
+    impl<'a> Decoder<'a> {
+        // Constructs a new decoder.
+        // Accepts data as a string slice,
+        // which then converts it to bytes to the underlying cursor.
+        pub fn new(data: &str) -> Decoder {
+            Decoder{ data: std::io::Cursor::new(data.as_bytes()) }
         }
 
-        pub fn decode_from<R>(reader: R) -> Result<Type> where R: std::io::Read {
-            unimplemented!()
+        // Read and advance from the cursor to the length of a passed buffer
+        // & save the data to it.
+        fn read(&mut self, buf: &mut [u8]) -> Result<(), DecodeError> {
+            match self.data.read(buf) {
+                Ok(n) if n == buf.len() => Ok(()),
+                _ => Err(DecodeError::EOF)
+            }
+        }
+
+        // Read & advance one byte from the cursor.
+        fn read_byte(&mut self) -> Result<u8, DecodeError> {
+            let mut buf = [0u8];
+            try!(self.read(&mut buf));
+            Ok(buf[0])
+        }
+
+        // Peeks, without advancing, one byte from the cursor.
+        fn peek_byte(&mut self) -> Result<u8, DecodeError> {
+            let data = self.read_byte()?;
+            let pos = self.data.position();
+            self.data.set_position(pos - 1);
+            Ok(data)
         }
     }
-}
 
-#[cfg(test)]
-mod test {
-    extern crate test;
 
-    use std;
-    use std::fs::File;
-    use ::bencode::{Parser, Type, Error};
+    #[cfg(test)]
+    mod test {
+        extern crate test;
 
-    /*
-        "Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'. 
-        For example i3e corresponds to 3 and i-3e corresponds to -3. 
-        Integers have no size limitation. 
-        i-0e is invalid. 
-        All encodings with a leading zero, such as i03e, are invalid,
-        other than i0e, which of course corresponds to 0."
+        use std;
+        use bencode::{Decoder, DecodeError, Value};
 
-        Source: http://www.bittorrent.org/beps/bep_0003.html
-    */
-    #[test]
-    fn parse_num() {
-        assert_eq!(Parser::decode_str("i0e").unwrap(), Type::Num(0));
-        assert_eq!(Parser::decode_str("i-1e").unwrap(), Type::Num(-1));
-        assert_eq!(Parser::decode_str("i28e").unwrap(), Type::Num(28));
-        assert_eq!(Parser::decode_str("i489e").unwrap(), Type::Num(489));
-        assert_eq!(Parser::decode_str("i981795470e").unwrap(), Type::Num(981795470));
+        /*
+            Tests the reading, advancing & peeking of data.
+        */
+        #[test]
+        fn read_and_peek() {
+            let data = "i3784e";
+            let mut decoder = Decoder::new(data);
+            assert_eq!(decoder.data.position(), 0);
 
-        assert_eq!(Parser::decode_str("i-0e").unwrap_err(), Error::Invalid);
-        assert_eq!(Parser::decode_str("i098e").unwrap_err(), Error::Invalid);
+            // Check, if reading of one byte advances the underlying cursor.
+            let mut byte = decoder.read_byte().unwrap();
+            assert_eq!(byte, 'i' as u8);
+            assert_eq!(decoder.data.position(), 1);
+
+            // Check, if peeking of one byte doesn't advance the underlying cursor.
+            byte = decoder.peek_byte().unwrap();
+            assert_eq!(byte, '3' as u8);
+            assert_eq!(decoder.data.position(), 1);
+        
+            // Read until the end & compare the expected with the position.
+            let mut buf = [0u8; 5];
+            let expected: &[u8] = "3784e".as_bytes();
+            decoder.read(&mut buf).unwrap();
+            assert_eq!(buf, expected);
+            assert_eq!(decoder.data.position(), data.len() as u64);
+
+            // Reading & peeking at the end should return an error.
+            assert_eq!(decoder.read_byte().unwrap_err(), DecodeError::EOF);
+            assert_eq!(decoder.peek_byte().unwrap_err(), DecodeError::EOF);
+        }
+
+        /*
+            "Integers are represented by an 'i' followed by the number in base 10 followed by an 'e'. 
+            For example i3e corresponds to 3 and i-3e corresponds to -3. 
+            Integers have no size limitation. 
+            i-0e is invalid. 
+            All encodings with a leading zero, such as i03e, are invalid,
+            other than i0e, which of course corresponds to 0."
+
+            Source: http://www.bittorrent.org/beps/bep_0003.html
+        */
+        #[test]
+        #[ignore]
+        fn parse_num() {
+            unimplemented!();
+        }
+
+        /*
+            "Strings are length-prefixed base ten followed by a colon and the string. 
+            For example 4:spam corresponds to 'spam'."
+
+            Source: http://www.bittorrent.org/beps/bep_0003.html
+        */
+        #[test]
+        #[ignore]
+        fn parse_str() {
+            unimplemented!();
+        }
+
+        /*
+            "Lists are encoded as an 'l' followed by their elements (also bencoded) followed by an 'e'. 
+            For example l4:spam4:eggse corresponds to ['spam', 'eggs']."
+
+            Source: http://www.bittorrent.org/beps/bep_0003.html
+        */
+        #[test]
+        #[ignore]
+        fn parse_list() {
+            unimplemented!();
+        }
+
+        /*
+            "Dictionaries are encoded as a 'd' followed by a list of alternating keys 
+            and their corresponding values followed by an 'e'. 
+            For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} 
+            and d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. 
+            Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics)."
+
+            Source: http://www.bittorrent.org/beps/bep_0003.html
+        */
+        #[test]
+        #[ignore]
+        fn parse_dict() {
+            unimplemented!();
+        }
     }
 
-    /*
-        "Strings are length-prefixed base ten followed by a colon and the string. 
-        For example 4:spam corresponds to 'spam'."
+    #[cfg(test)]
+    mod bench {
 
-        Source: http://www.bittorrent.org/beps/bep_0003.html
-    */
-    #[test]
-    fn parse_str() {
-        unimplemented!();
-    }
-
-    /*
-        "Lists are encoded as an 'l' followed by their elements (also bencoded) followed by an 'e'. 
-        For example l4:spam4:eggse corresponds to ['spam', 'eggs']."
-
-        Source: http://www.bittorrent.org/beps/bep_0003.html
-    */
-    #[test]
-    fn parse_list() {
-        unimplemented!();
-    }
-
-    /*
-        "Dictionaries are encoded as a 'd' followed by a list of alternating keys 
-        and their corresponding values followed by an 'e'. 
-        For example, d3:cow3:moo4:spam4:eggse corresponds to {'cow': 'moo', 'spam': 'eggs'} 
-        and d4:spaml1:a1:bee corresponds to {'spam': ['a', 'b']}. 
-        Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics)."
-
-        Source: http://www.bittorrent.org/beps/bep_0003.html
-    */
-    #[test]
-    fn parse_dict() {
-        unimplemented!();
-    }
-}
-
-#[cfg(test)]
-mod bench {
-    extern crate test;
-    use ::bencode::{Parser};
-
-    #[bench]
-    fn bench_parse_num(b: &mut test::Bencher) {
-        b.iter(|| Parser::decode_str("i175476243e").unwrap());
     }
 }
