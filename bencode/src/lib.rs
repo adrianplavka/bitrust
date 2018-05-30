bbb7f133893162671d799856a3240a82fee05910
Adrian Plavka <adrian.plavka@gmail.com>
2018-05-30 22:39:42 +0200

    Added serde's package as an extern crate

--- bencode/src/lib.rs
+++ bencode/src/lib.rs
@@ -1,6 +1,17 @@
 
+// TODO: Remove allowances after successful implementation.
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
+#[macro_use]
+extern crate serde;
+
+#[doc(inline)]
+pub use self::error::{Error, Result};
 #[doc(inline)]
 pub use self::decoder::*;
 
-pub mod decoder;
 pub mod error;
+pub mod decoder;
+pub mod de;
