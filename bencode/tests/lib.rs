2d2f411190007f11bbfcf561cc25c8ee59e59f6e
Adrian Plavka <adrian.plavka@goodvision.sk>
2023-03-28 19:35:26 +0200

    Add circular property-based test for Bencode serialization & deserialization

--- bencode/tests/lib.rs
+++ bencode/tests/lib.rs
@@ -1,14 +1,77 @@
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+
+    use arbitrary::{Arbitrary, Unstructured};
     use quickcheck_macros::quickcheck;
 
     use bitrust_bencode::{from_str, to_string};
+    use rand::Rng;
+    use serde_derive::{Deserialize, Serialize};
+
+    #[derive(Deserialize, Serialize, Eq, PartialEq, Debug)]
+    #[serde(untagged)]
+    enum Value {
+        Num(usize),
+
+        String(String),
+
+        List(Vec<Value>),
+
+        Map(HashMap<String, Value>),
+    }
+
+    impl Value {
+        fn _arbitrary<'a>(
+            u: &mut arbitrary::Unstructured<'a>,
+            rng: &mut rand::rngs::ThreadRng,
+            depth: u8,
+        ) -> arbitrary::Result<Value> {
+            if depth >= 3 {
+                return Ok(Value::Num(u.arbitrary()?));
+            }
+
+            match rng.gen_range(0, 10) {
+                0 => Ok(Value::Num(u.arbitrary()?)),
+                1 => Ok(Value::String(u.arbitrary()?)),
+                2 => {
+                    let size = rng.gen_range(0, 10);
+                    let mut vec = Vec::with_capacity(size);
+
+                    for _ in 0..size {
+                        vec.push(Self::_arbitrary(u, rng, depth + 1)?);
+                    }
+
+                    Ok(Value::List(vec))
+                }
+                _ => {
+                    let size = rng.gen_range(0, 10);
+                    let mut map = HashMap::with_capacity(size);
+
+                    for _ in 0..size {
+                        map.insert(u.arbitrary()?, Self::_arbitrary(u, rng, depth + 1)?);
+                    }
+
+                    Ok(Value::Map(map))
+                }
+            }
+        }
+    }
+
+    impl<'a> Arbitrary<'a> for Value {
+        fn arbitrary(u: &mut arbitrary::Unstructured<'a>) -> arbitrary::Result<Self> {
+            Self::_arbitrary(u, &mut rand::thread_rng(), 0)
+        }
+    }
 
     #[quickcheck]
-    fn circular(value: String) {
-        let bencode = to_string(&value).unwrap();
-        let de_value = from_str::<&str>(&bencode).unwrap();
+    fn circular_property(fuzz_data: Vec<u8>) {
+        let mut u = Unstructured::new(fuzz_data.as_slice());
+        let value = Value::arbitrary(&mut u).unwrap();
 
-        assert_eq!(value, de_value);
+        assert_eq!(
+            value,
+            from_str::<Value>(&to_string(&value).unwrap()).unwrap()
+        );
     }
 }
